0e52cc3f876eefdf6b0086581730db7bc362d3dd
frfn <flexer.fadrigalan@gmail.com>
2020-05-11 15:24:26 -0400

    adding counter with React Hooks test

---
+++ src/TestFiles/CounterWithReactHooks.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
+/*
+Left Off At – Mock API
+https://hub.packtpub.com/getting-started-with-react-hooks-by-building-a-counter-with-usestate-and-useeffect/ 
+*/
+
+const INCREMENT = [1, 2, 5, 10]
+
+const Counter = () => {
+    const [ counter, setCounter ] = useState(0);
+
+    useEffect(() => {
+        document.title = `Counter: ${counter}`;
+    })
+
+    const add = (value) => {
+        setCounter(prevCounter => {
+        return prevCounter + value
+        })
+    }
+
+    return(
+        <div>
+            <h1> Counter: {counter} </h1>
+            {INCREMENT.map(num => 
+                <button onClick={() => add(num)}> {num} </button>
+            )}
+        </div>
+    );
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Container/App';
+import TF from './TestFiles/CounterWithReactHooks';
 import * as serviceWorker from './Container/serviceWorker';
 
 ReactDOM.render(
